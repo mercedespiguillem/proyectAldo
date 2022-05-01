bf4ff5a670682fa45ab576da0ec97aa431ec47fa
Maria Mercedes Piguillem <mercedes.piguillem@gmail.com>
2022-04-30 22:23:50 -0300

    Formulario definitivo, cambio de detalles(dropdown difusion, colores de fuentes)

--- app.js
+++ app.js
@@ -1,76 +1,89 @@
 // FORM DE CONTACTO QUE APARECE AL HACER CLIC EN EL BOTON CONTACTANOS
 
-let boton = document.getElementById("btnSubmit");
-boton.addEventListener("click", showForm);
+// let boton = document.getElementById("btnSubmit");
+// boton.addEventListener("click", showForm);
 
-const element = document.getElementById("act2");
+// const element = document.getElementById("act2");
 
-function showForm(e) {
-  e.preventDefault();
-  createForm(element);
-}
+// function showForm(e) {
+//   e.preventDefault();
 
-function createForm(element) {
-  if (element.childNodes[0]) {
-    element.removeChild(element.childNodes[2]);
+//   createForm(element);
+// }
 
-    const formulary = document.createElement("form");
+// function createForm(element) {
+//   const formulary = document.createElement("form");
 
-    formulary.id = "form";
-    formulary.className = "card m-3 p-5";
-    formulary.innerHTML = `<div class="container-fluid">
-  <h2 class="titles mb-4">
-    Envianos tus datos para realizar la inscripción
-  </h2>
-      <div class="mb-3">
-        <input required
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nombre y apellido"
-            class="form-control form-control-lg m-2">
-      </div>
-      <div class="mb-3">
-        <input required
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            class="form-control form-control-lg m-2">
-      </div>
-      <div class="mb-3">
-        <input
-        required
-        type="number"
-        name="dni"
-        id="dni"
-        placeholder="Número de documento"
-        class="form-control form-control-lg m-2"/>
-      </div>  
-      <div class="mb-3">
-        <input
-        required
-        type="number"
-        name="phone"
-        id="phone"
-        placeholder="Número de teléfono"
-        class="form-control form-control-lg m-2"/>
-      </div>  
-      <div class="mb-3">
-        <textarea
-        required
-        type="text"
-        name="message"
-        id="message"
-        placeholder="Escriba aquí su mensaje"
-        class="form-control form-control-lg m-2"></textarea>
-      </div>
-      <input type="submit" id="button" value="Enviar mensaje" class="btn btn-secondary m-3"/>
-    </div>`;
+//   // if (element.childNodes[0]) {
+//   //   element.removeChild(element.childNodes[2]);
+//   // }
 
-    element.appendChild(formulary);
-  }
-}
+//   formulary.id = "form1";
+//   formulary.className = "card m-3 p-5";
+//   formulary.innerHTML = `<div class="container-fluid">
+//     <h2 class="titles mb-4">
+//     Envianos tus datos para realizar la inscripción
+//     </h2>
+//     <div>
+//       <div class="mb-3">
+//         <input required
+//             type="text"
+//             name="name"
+//             id="name"
+//             placeholder="Nombre y apellido"
+//             class="form-control form-control-lg m-2">
+//       </div>
+//       <div class="mb-3">
+//         <input
+//         required
+//         type="text"
+//         name="profession"
+//         id="profession"
+//         placeholder="Profesión"
+//         class="form-control form-control-lg m-2"/>
+//       </div> 
+//       <div class="mb-3">
+//         <input
+//         required
+//         type="text"
+//         name="job"
+//         id="job"
+//         placeholder="Ocupación actual"
+//         class="form-control form-control-lg m-2"/>
+//       </div>  
+//       <div class="mb-3">
+//         <input
+//         required
+//         type="text"
+//         name="country"
+//         id="country"
+//         placeholder="País"
+//         class="form-control form-control-lg m-2"/>
+//       </div> 
+//       <div class="mb-3">
+//         <input required
+//             type="email"
+//             name="email"
+//             id="email"
+//             placeholder="Email"
+//             class="form-control form-control-lg m-2">
+//       </div>
+      
+//       <div class="mb-3">
+//         <textarea
+//         type="text"
+//         name="message"
+//         id="message"
+//         placeholder="Escriba aquí su mensaje"
+//         class="form-control form-control-lg m-2"></textarea>
+//       </div>
+//       <input type="submit" id="button" value="Enviar mensaje" class="btn btn-secondary m-3"/>
+//     </div>
+//   </div>`;
+
+//   element.appendChild(formulary);
+// }
+
 // JS DE EMAIL JS, CAMBIO DE ESTADO DEL BOTON Y ENVIO DEL MENSAJE
 
 const btn = document.getElementById("button");
